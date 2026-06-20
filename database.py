67d86301a3dfbbbedaa01ce905f4625db77ffa5c
MohammedDev-yt <animeworldday2005@gmail.com>
2026-06-20 19:10:26 +0530

    database.py

--- database.py
+++ database.py
@@ -18,10 +18,9 @@ import time
 client = AsyncIOMotorClient(MONGO_URI)
 
 db = client.rename_bot
-
 users = db.users
-
 leaderboard = db.leaderboard
+user_bots = db.user_bots
 
 
 # ------------------------- #
@@ -137,6 +136,80 @@ async def get_premium_status(uid):
 async def setup_database():
     print("Dᴀᴛᴀʙᴀsᴇ Cᴏɴɴᴇᴄᴛᴇᴅ ✅")
 
+# ---------------- USER BOTS SYSTEM ---------------- #
+
+async def get_user_bots(user_id):
+    return await user_bots.find_one({"_id": user_id})
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+async def add_user_bot(user_id, username, token):
+
+    await user_bots.update_one(
+        {"_id": user_id},
+        {
+            "$push": {
+                "bots": {
+                    "username": username,
+                    "token": token,
+                    "uploads": 0
+                }
+            },
+            "$setOnInsert": {
+                "active": 0,
+                "last_used": "Never"
+            }
+        },
+        upsert=True
+    )
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+async def increase_bot_upload(user_id, bot_index=0):
+
+    await user_bots.update_one(
+        {"_id": user_id},
+        {
+            "$inc": {
+                f"bots.{bot_index}.uploads": 1
+            },
+            "$set": {
+                "last_used": "Just now"
+            }
+        }
+    )
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+async def set_active_bot(user_id, index):
+
+    await user_bots.update_one(
+        {"_id": user_id},
+        {
+            "$set": {
+                "active": index
+            }
+        }
+    )
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+async def delete_all_bots(user_id):
+
+    await user_bots.delete_one({"_id": user_id})
+    
 # ------------------------- #
 # Don't Remove Credit 
 # Ask Doubt @AU_Bot_Discussion 
